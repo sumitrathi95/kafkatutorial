ec827197c26d5917da320c31fb540c7f20481e81
Sumit Rathi <sumitpravin.rathi@karkinos.in>
2020-09-29 11:14:36 +0530

    Initial Commit

---
+++ src/test/java/com/codeaches/kafka/basics/KafkaProducerConsumerBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeaches.kafka.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerConsumerBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
